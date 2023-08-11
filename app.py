44e3aef7a79e56b0d0aeaf707369557aed23f577
anngomez130 <125148687+anngomez130@users.noreply.github.com>
2023-08-11 14:31:58 -0400

    Update app.py - bgimage variable and print url
    Update bgimage variable and printing of background image url

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from pymysql import connections
 import os
 import random
 import argparse
+import boto3
 
 
 app = Flask(__name__)
 
 # Initialize Amazon S3 client
 s3 = boto3.client('s3')
+
+# Get Background Image URL from ConfigMap
+BGIMAGE = os.environ.get('BGIMAGE', 'interstellar.jpg')
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
@@ -50,9 +54,7 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lim
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    # Get Background Image URL from ConfigMap
-    bg_image = os.environ.get('BGIMAGE', 'interstellar.jpg')
-    return render_template('addemp.html', color=color_codes[COLOR], bgimage=bg_image)
+    return render_template('addemp.html', color=color_codes[COLOR], bgimage=BGIMAGE)
 
 @app.route("/about", methods=['GET','POST'])
 def about():
@@ -137,5 +139,8 @@ if __name__ == '__main__':
     if COLOR not in color_codes:
         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
         exit(1)
+
+    # Print Background Image URL
+    print("Background Image URL:", BGIMAGE)  # Print the background image URL
 
     app.run(host='0.0.0.0',port=81,debug=True)
